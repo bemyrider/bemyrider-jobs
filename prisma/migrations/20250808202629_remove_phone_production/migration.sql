96ea6deaf54169499fe1b7735c65b056f97a1c94
bemyrider <bemyrider@gmail.com>
2025-08-08 22:42:11 +0200

    Fix: Restore phone field in application forms and database

--- prisma/migrations/20250808202629_remove_phone_production/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `phone` on the `applications` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."applications" DROP COLUMN "phone";
