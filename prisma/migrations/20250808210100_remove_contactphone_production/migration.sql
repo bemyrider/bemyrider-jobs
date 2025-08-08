1bdb50338bebf6a632e062fa26be207302561b17
bemyrider <bemyrider@gmail.com>
2025-08-08 23:01:20 +0200

    Fix: Remove contactphone field from production database

---
+++ prisma/migrations/20250808210100_remove_contactphone_production/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `contactphone` on the `job_offers` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."job_offers" DROP COLUMN "contactphone";
